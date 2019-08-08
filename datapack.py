7f895e20cf718dfc59fecab0be90733181489932
wangyao <wangyao.dream@hotmail.com>
2019-08-08 21:05:33 +0800

    first version

--- datapack.py
+++ datapack.py
@@ -1,59 +1,167 @@
 import xlwt
+import datetime
 import json
 
 NAME_MAP = {'SearchIndex': '搜索指数', 'FeedIndex': '资讯指数', 'NewsIndex': '媒体指数'}
 
 class Pack:
-    def __init__(self, tag_name, tag_data, tag_path, keyword):
+    def __init__(self, tag_data, tag_path, keyword):
         """
         初始化pack对象，直接将数据导出成excel
-        :param tag_name: 文件名
         :param tag_data: 目标数据
         :param tag_path: 存储源路径
         :param keyword: 关键字
         """
-        self._tag_name = tag_name
         self._tag_data = tag_data
         self._tag_path = tag_path
         self._keyword = keyword  # keyword用来制作表头的字符串
+        self.fmt_data()  # 运行存储
+
 
     def fmt_data(self):
-        # 获取索要构造的数据
-        tag_data = self._tag_data
-        filename = '{}.xls'.format(NAME_MAP[self._tag_name])
-        filepath = '{}/{}'.format(self._tag_path, filename)
-        main_workbook = xlwt.Workbook(encoding='utf-8')
+        # 存储搜索指数
+        self.searchIndexProcess()
+        # 存储咨询指数
+        self.feedIndexProcess()
+        # 存储媒体指数
+        self.newsIndexProcess()
+        
 
     def searchIndexProcess(self):
-        searchIndexData = self._tag_data['SearchIndex']
-        searchIndexPath = '{}/{}/'.format(self._tag_path, )
-        process_data = []
-        for i in searchIndexData['all']:
-            process_data.append([i['date'], i['index']])
-        pc_count = 0
-        for j in searchIndexData['pc']:
-            process_data[pc_count].append(j['index'])
-            pc_count += 1
-        wise_count = 0
-        for k in searchIndexData['wise']:
-            process_data[wise_count].append(k['index'])
-            wise_count += 1
+        """这个代表searchIndex指数的所有数据，所以应该是它来创建excel对象并储存
+            在使用的self._tag_data 是一个整体数据的集合，使用的时候要注意
+        """
+        workbook = xlwt.Workbook(encoding='utf-8')
+        filename = '{}_搜索指数.xls'.format(datetime.datetime.now().strftime('%y%m%d%m%S'))
+        national_titles = '日期 {}_整体 {}_PC {}_移动'.format(self._keyword, self._keyword, self._keyword).split()
+        self.national_sheet(workbook, national_titles, self._tag_data['National'], 'SearchIndex')
+        province_titles = '日期 省份 {}_整体 {}_PC {}_移动'.format(self._keyword, self._keyword, self._keyword, self._keyword).split()
+        self.provinces_sheet(workbook, province_titles, self._tag_data['Province'], 'SearchIndex')
+        city_titles = '日期 城市名 {}_整体 {}_PC {}_移动'.format(self._keyword, self._keyword, self._keyword, self._keyword).split()
+        self.citys_sheet(workbook, city_titles, self._tag_data['City'], 'SearchIndex')
+        # save file
 
+        file_path = '{}/{}/{}'.format(self._tag_path, '搜索指数', filename)
+        workbook.save(file_path)
 
+    def feedIndexProcess(self):
+        """资讯指数数据存储成excel"""
+        workbook = xlwt.Workbook(encoding='utf-8')
+        filename = '{}_资讯指数.xls'.format(datetime.datetime.now().strftime('%y%m%d%m%S'))
+        # 资讯指数也分全国、省份、市级,但是不区分移动或pc
+        national_titles = '日期 {}_资讯_指数'.format(self._keyword).split()
+        self.national_sheet(workbook, national_titles, self._tag_data['National'], 'FeedIndex', False)
+        province_titles = '日期 省份 {}_资讯_指数'.format(self._keyword).split()
+        self.provinces_sheet(workbook, province_titles, self._tag_data['Province'], 'FeedIndex', False)
+        city_titles = '日期 城市名 {}_资讯_指数'.format(self._keyword).split()
+        self.citys_sheet(workbook, city_titles, self._tag_data['City'], 'FeedIndex', False)
+        # save file
+        file_path = '{}/{}/{}'.format(self._tag_path, '资讯指数', filename)
+        workbook.save(file_path)
+
+    def newsIndexProcess(self):
+        """媒体指数"""
+        workbook = xlwt.Workbook(encoding='utf-8')
+        filename = '{}_{}_媒体指数.xls'.format(datetime.datetime.now().strftime('%y%m%d%m%S'), self._keyword)
+        # 资讯指数也分全国、省份、市级,但是不区分移动或pc
+        national_titles = '日期 {}_媒体_指数'.format(self._keyword).split()
+        self.national_sheet(workbook, national_titles, self._tag_data['National'], 'NewsIndex', False)
+        province_titles = '日期 省份 {}_媒体_指数'.format(self._keyword).split()
+        self.provinces_sheet(workbook, province_titles, self._tag_data['Province'], 'NewsIndex', False)
+        city_titles = '日期 城市名 {}_媒体_指数'.format(self._keyword).split()
+        self.citys_sheet(workbook, city_titles, self._tag_data['City'], 'NewsIndex', False)
+        # save file
+        file_path = '{}/{}/{}'.format(self._tag_path, '媒体指数', filename)
+        workbook.save(file_path)
 
     @staticmethod
-    def data2excel(_sheetname: str, _title: list, _data: list, _workbook: xlwt.Workbook):
-        # 这里获取的data应该是格式化好的符合excel结构的数据
-        worksheet = _workbook.add_sheet(_sheetname)
-        # 构造表的标题行
-        for i in range(len(_title)):
-            worksheet.write(0, i, _title[i])
-        # 给表填充数据
-        row = 1
-        for j in range(len(_data)):
-            for n in range(len(_data[j])):
-                worksheet.write(j+1, n, _data[j][n])
-            row += 1
+    def init_sheet(_worksheet, title):
+        for i in range(len(title)):
+            _worksheet.write(0, i, title[i])
+
+    @staticmethod
+    def national_sheet(workbook: xlwt.Workbook, titles: list, tag_data, item_name, specific=True):
+        worksheet = workbook.add_sheet('全国')
+        # 初始化构造title
+        Pack.init_sheet(worksheet, titles)
+        #填充内容
+        fill_row = 1
+        for _ in tag_data[item_name]['all']:
+            if specific:
+                temp_data = [
+                    tag_data[item_name]['all'][fill_row - 1]['date'],
+                    tag_data[item_name]['all'][fill_row - 1]['index'],
+                    tag_data[item_name]['pc'][fill_row - 1]['index'],
+                    tag_data[item_name]['wise'][fill_row - 1]['index'],
+                ]
+            else:
+                temp_data = [
+                    tag_data[item_name]['all'][fill_row - 1]['date'],
+                    tag_data[item_name]['all'][fill_row - 1]['index'],
+                ]
+            for j in range(len(temp_data)):
+                worksheet.write(fill_row, j, temp_data[j])
+            fill_row += 1
+
+    @staticmethod
+    def provinces_sheet(workbook: xlwt.Workbook, titles, tag_data, item_name, specific=True):
+        worksheet = workbook.add_sheet('省份')
+        # init title
+        Pack.init_sheet(worksheet, titles)
+        # fill data
+        fill_row = 1
+        for tag in tag_data:
+            cursor_index = 0
+            for _ in tag_data[tag][item_name]['all']:
+                if specific:
+                    temp_data = [
+                        tag_data[tag][item_name]['all'][cursor_index]['date'],
+                        tag,
+                        tag_data[tag][item_name]['all'][cursor_index]['index'],
+                        tag_data[tag][item_name]['pc'][cursor_index]['index'],
+                        tag_data[tag][item_name]['wise'][cursor_index]['index'],
+                    ]
+                else:
+                    temp_data = [
+                        tag_data[tag][item_name]['all'][cursor_index]['date'],
+                        tag,
+                        tag_data[tag][item_name]['all'][cursor_index]['index'],
+                    ]
+                for j in range(len(temp_data)):
+                    worksheet.write(fill_row, j, temp_data[j])
+                fill_row += 1
+                cursor_index += 1
+
+    @staticmethod
+    def citys_sheet(workbook: xlwt.Workbook, titles, tag_data, item_name, specific=True):
+        worksheet = workbook.add_sheet('市级')
+        # init title
+        Pack.init_sheet(worksheet, titles)
+        # fill data
+        fill_row = 1
+        for tag in tag_data:
+            cursor_index = 0
+            for _ in tag_data[tag][item_name]['all']:
+                if specific:
+                    temp_data = [
+                        tag_data[tag][item_name]['all'][cursor_index]['date'],
+                        tag,
+                        tag_data[tag][item_name]['all'][cursor_index]['index'],
+                        tag_data[tag][item_name]['pc'][cursor_index]['index'],
+                        tag_data[tag][item_name]['wise'][cursor_index]['index'],
+                    ]
+                else:
+                    temp_data = [
+                        tag_data[tag][item_name]['all'][cursor_index]['date'],
+                        tag,
+                        tag_data[tag][item_name]['all'][cursor_index]['index'],
+                    ]
+                for j in range(len(temp_data)):
+                    worksheet.write(fill_row, j, temp_data[j])
+                fill_row += 1
+                cursor_index += 1
+
+
 
 
 
--- getindex.py
+++ getindex.py
@@ -1,9 +1,14 @@
 from urllib.parse import urlencode
 from collections import defaultdict
+import random
+import os
+import threading
 import time
 import datetime
 import requests
 import json
+
+import datapack
 
 import datapack as dp
 # import pandas as pd
@@ -23,8 +28,24 @@ headers = {
 }
 
 
+class MyThread(threading.Thread):
+    def __init__(self, func, args=()):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        try:
+            return self.result
+        except Exception:
+            return None
+
+
 class BaiduIndex:
-    def __init__(self, tag_url, keywords, start_date, end_date, area=0):
+    def __init__(self, cookie, tag_url, keywords, start_date, end_date, area=0):
         """
         初始化BaiduIndex对象，赋予一些基本属性
         :param keywords: 关键词
@@ -32,6 +53,7 @@ class BaiduIndex:
         :param end_date: 结束日期
         :param area: 区域，默认0代表全国（地区代码需要自己总结）
         """
+        self._cookie = cookie
         self._keywords = keywords if isinstance(keywords, list) else keywords.split(',')
         self._time_range_list = self.get_time_range_list(start_date, end_date)
         self._all_kind = ['all', 'pc', 'wise']
@@ -68,7 +90,7 @@ class BaiduIndex:
             'area': self._area,
         }
         # 获取页面数据
-        html = self.http_get(tag_url)
+        html = self.http_get(tag_url, self._cookie)
         datas = json.loads(html)
         uniqid = datas['data']['uniqid']
         encrypt_datas = []
@@ -82,7 +104,7 @@ class BaiduIndex:
 
     def get_key(self, uniqid):
         tag_url = 'http://index.baidu.com/Interface/api/ptbk?uniqid=%s' % uniqid
-        html = self.http_get(tag_url)
+        html = self.http_get(tag_url, self._cookie)
         datas = json.loads(html)
         key = datas['data']
         return key
@@ -123,11 +145,9 @@ class BaiduIndex:
         return self.result[keyword][kind]
 
     @staticmethod
-    def http_get(url):
+    def http_get(url, cookie):
         # 获取已经存储好的cookie
-        with open('cookies', 'r') as f:
-            cookies = f.read()
-        headers['Cookie'] = cookies
+        headers['Cookie'] = cookie
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.text
@@ -190,51 +210,110 @@ class Tag:
         return tag_urls
 
 
-def getNationalData(keyword, startDate, endDate):
+def getNationalData(cookie, keyword, startDate, endDate):
     # 获取全国的数据
     urls = Tag.structure_urls(keyword, startDate, endDate, 0)
     result_data = {}
     for i in urls:
-        bi = BaiduIndex(urls[i], [keyword], startDate, endDate, 0)
+        bi = BaiduIndex(cookie, urls[i], [keyword], startDate, endDate, 0)
         result_data[i] = dict(bi.result[keyword])
     return result_data
 
 
-def getProvinceData(keyword, startDate, endDate, provinceMap):
+def getProvinceData(cookie, keyword, startDate, endDate, provinceMap):
     result_data = {}
+    out_thread = []
     for curr_province in provinceMap:
         # time.sleep(0.1)
+        thread_list = []
         temp_data = {}
         province_name = curr_province
         province_code = provinceMap[curr_province]
         _urls = Tag.structure_urls(keyword, startDate, endDate, province_code)
         for i in _urls:
-            bi = BaiduIndex(_urls[i], [keyword], startDate, endDate, province_code)
+            # 单线程
+            bi = BaiduIndex(cookie, _urls[i], [keyword], startDate, endDate, province_code)
             temp_data[i] = dict(bi.result[keyword])
+
+        #     # 多线程
+        #     t = MyThread(BaiduIndex, args=(cookie, _urls[i], [keyword], startDate, endDate, province_code))
+        #     thread_list.append((i, t))
+        #     t.start()
+        # for item_name, t in thread_list:
+        #     t.join()
+        #     res = t.get_result()
+        #     temp_data[item_name] = dict(res.result[keyword])
         result_data[province_name] = temp_data
     return result_data
 
-def main(keyword, startDate, endDate):
+
+def getCitysData(cookie, keyword, startDate, endDate, citysMap):
+    result_data = {}
+    citys_data = {}
+    for i in citysMap:
+        citys_data.update(citysMap[i])
+    for city in citys_data:
+        thread_list = []
+        temp_data = {}
+        city_code = citys_data[city]
+        _urls = Tag.structure_urls(keyword, startDate, endDate, city_code)
+        for i in _urls:
+            # 单线程
+            bi = BaiduIndex(cookie, _urls[i], [keyword], startDate, endDate, city_code)
+            temp_data[i] = dict(bi.result[keyword])
+
+        #     # 多线程
+        #     t = MyThread(BaiduIndex, args=(cookie, _urls[i], [keyword], startDate, endDate, city_code))
+        #     thread_list.append((i, t))
+        #     t.start()
+        # for item_name, t in thread_list:
+        #     t.join()
+        #     res = t.get_result()
+        #     if res:
+        #         temp_data[item_name] = dict(res.result[keyword])
+        result_data[city] = temp_data
+    return result_data
+
+def main(keyword, startDate, endDate, target_path='.'):
     # 设定几个需要获取指数的url并逐个进行数据爬取，最后再进行封装存储成excel文件
     with open('area_data/provinces_code.json', 'r', encoding='utf-8') as f_provinces:
         provinces = json.load(f_provinces)
-    with open('area_data/citys_code.json', 'r', encoding='utf-8') as f_cities:
+    with open('area_data/citys_code_bak.json', 'r', encoding='utf-8') as f_cities:
         cities = json.load(f_cities)
-    taget_data = {}
-    national_data = getNationalData(keyword, startDate, endDate)
-    provinces_data = getProvinceData(keyword, startDate, endDate, provinces)
-    with open('/Users/wangyao/Desktop/temp_data/province_data.json', 'w', encoding='utf-8') as f:
-        f.write(json.dumps(provinces_data))
+    # 获取cookie
+    with open('cookies', 'r') as f:
+        cookies = []
+        for i in f:
+            cookies.append(i.replace('\n', ''))
+    # 应该创建所需要的路径目录
+    if not os.path.exists(target_path):
+        # 创建主目录
+        if target_path != '.':
+            os.mkdir(target_path)
+        sub_dirs = "搜索指数 资讯指数 媒体指数".split()
+        for i in sub_dirs:
+            os.mkdir(target_path + '/{}/'.format(i))
+    # 构造一个主集合
+    target_data = dict()
+    # print('正在获取全国数据...')
+    # target_data['National'] = getNationalData(cookies[0],keyword, startDate, endDate)
+    # print('正在获取省份数据...')
+    # target_data['Province'] = getProvinceData(cookies[0], keyword, startDate, endDate, provinces)
+    print('正在获取市级数据...')
+    target_data['City'] = getCitysData(cookies[2], keyword, startDate, endDate, cities)
+    with open('temp/tempdata.json', 'w', encoding='utf-8') as fp:
+        fp.write(json.dumps(target_data))
+    print('开始导出数据...')
+    # pack = datapack.Pack(target_data, target_path, keyword)
+    # 将数据导出成excel文件
+    # with open('/Users/wangyao/Desktop/temp_data/test_data.json', 'w', encoding='utf-8') as f:
+    #     f.write(json.dumps(target_data['Province']))
     print('Done!')
-
-    
-
-
-
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    main('股票', '2019-06-30', '2019-08-01')
-    end_time = time.time() - start_time
-    print('耗时 {} s'.format(end_time))
+    # start_time = time.time()
+    # main('上市', '2017-06-30', '2019-08-01', '/Users/wangyao/Desktop/result')
+    # end_time = time.time() - start_time
+    # print('耗时 {} s'.format(end_time))
+
